f8ce2fb882b5a208fc607dc0171f5f6f0b6f3812
YutaGoto <you.goto.510@gmail.com>
2014-03-15 17:39:49 +0900

    edit manifest

--- draw.js
+++ draw.js
@@ -1,11 +1,9 @@
-// alert(event.screenX);
-// alert("hello");
 $(function(){
 	$("html").click(function(e){
 		var element=document.getElementById("id");
 		element.style.position='absolute';
-		// element.style.left=e.clientX-10+'px';
-		// element.style.top=e.clientY-10+'px';
+		$("#id").animate({"left" : e.clientX+(Math.random()-0.5)*400+'px', "top" : e.clientY+(Math.random()-0.5)*400+'px'}, "fast");
+		$("#id").animate({"left" : e.clientX+(Math.random()-0.5)*400+'px', "top" : e.clientY+(Math.random()-0.5)*400+'px'}, "fast");
 		$("#id").animate({"left" : e.clientX-10+'px', "top" : e.clientY-10+'px'}, "fast");
 	});
 });
