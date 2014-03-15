a145cab1f607f2a5a51ec5ea72d86e84724f15df
YutaGoto <you.goto.510@gmail.com>
2014-03-15 15:58:52 +0900

    add animation

--- draw.js
+++ draw.js
@@ -4,7 +4,8 @@ $(function(){
 	$("html").click(function(e){
 		var element=document.getElementById("id");
 		element.style.position='absolute';
-		element.style.left=e.clientX-10+'px';
-		element.style.top=e.clientY-10+'px';
+		// element.style.left=e.clientX-10+'px';
+		// element.style.top=e.clientY-10+'px';
+		$("#id").animate({"left" : e.clientX-10+'px', "top" : e.clientY-10+'px'}, "fast");
 	});
 });
